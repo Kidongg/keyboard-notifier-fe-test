9071b2b505f8b66a740eed12fef7fd90d96581a4
Kidongg <dlrlehd7@daum.net>
2024-09-22 16:09:28 +0900

    chore: github action 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+
 cd ../
 mkdir output
-cp -R ./team-repository-name/* ./output
-cp -R ./output ./team-repository-name/
+cp -R ./keyboard-notifier-fe-test/* ./output
+cp -R ./output ./keyboard-notifier-fe-test/
