196f6351b19c8125e6256e0784b2124937fb2183
Kidongg <dlrlehd7@daum.net>
2024-09-22 16:16:23 +0900

    chore: github action 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 cd ../
-mkdir -p output
 
-# 'output' 디렉토리를 제외하고 모든 파일을 복사
-rsync -av --exclude='output' ./keyboard-notifier-fe-test/ ./output/
-
-# mkdir output
-# cp -R ./keyboard-notifier-fe-test/* ./output
-# cp -R ./output ./keyboard-notifier-fe-test/
+mkdir output
+cp -R ./keyboard-notifier-fe-test/* ./output
+cp -R ./output ./keyboard-notifier-fe-test/
